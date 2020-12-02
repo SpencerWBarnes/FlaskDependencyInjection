c8fc4e6ee6d38c2b2a8543054a983d73db0eaea9
Spencer Barnes <interstellarjumper@gmail.com>
2020-12-02 16:39:27 -0600

    Added config test

--- SampleApp/tests.py
+++ SampleApp/tests.py
@@ -1,15 +1,22 @@
 import unittest
 from unittest.mock import Mock
 
+from flask import Response
+
 import app
 from WorkerClass import WorkerClass
 from WrapperClass import WrapperClass
+"""
+Major difficulty with imports encountered. ref https://exceptionshub.com/relative-imports-in-python-3.html
+Current solution is to place the tests in the same dir as the tested material.
+"""
+
 
 class TestTest(unittest.TestCase):
   app = None
 
   def setUp(self):
-    self.app = app.buildApp()    
+    self.app = app.buildApp()
     self.client = self.app.test_client()
 
   def tearDown(self):
@@ -18,28 +25,28 @@ class TestTest(unittest.TestCase):
   def test_WrapperRunTask_StandardCall_200(self):
     # Input
     workerMock = Mock(WorkerClass)
-    expectedData = b"test"
+    expectedData = "test"
     expectedCode = 200
     workerMock.executeTask.return_value = expectedData
 
     # Execute
     with self.app.container.workerObj.override(workerMock):
-      result = self.client.get("/api/tester")
+      result: Response = self.client.get("/api/tester")
 
     # Evaluate
-    self.assertEqual(result.get_data(), expectedData)
+    self.assertEqual(result.get_data(as_text=True), expectedData)
     self.assertEqual(result.status_code, expectedCode)
     workerMock.executeTask.assert_called_once()
 
   def test_EndpointApiTester_StandardCall_200(self):
     # Input
     wrapperMock = Mock(WrapperClass)
-    expectedData = b"test"
+    expectedData = "test"
     expectedCode = 200
     wrapperMock.runTask.return_value = expectedData
 
     workerMock = Mock(WorkerClass)
-    workerMock.executeTask.return_value = b"Bad call"
+    workerMock.executeTask.return_value = "Bad call"
     self.app.container.workerObj.override(workerMock)
 
     # Execute
@@ -47,11 +54,26 @@ class TestTest(unittest.TestCase):
       result = self.client.get("/api/tester")
 
     # Evaluate
-    self.assertEqual(result.get_data(), expectedData)
+    self.assertEqual(result.get_data(as_text=True), expectedData)
     self.assertEqual(result.status_code, expectedCode)
 
     wrapperMock.runTask.assert_called_once() # wrapper was hit
     workerMock.executeTask.assert_not_called() # underlying worker was not hit
+
+  def test_ConfigData_StandardCall_200(self):
+    # Input
+    expectedDataPrefix = "expected"
+    expectedData = expectedDataPrefix + " 1"
+    expectedCode = 200
+    configOverride = {'testing': {'dummyData': expectedDataPrefix}}
+    
+    # Execute
+    with self.app.container.config.override(configOverride):
+      result = self.client.get("/api/tester")
+
+    # Evaluate
+    self.assertEqual(result.get_data(as_text=True), expectedData)
+    self.assertEqual(result.status_code, expectedCode)
 
 if (__name__ == "__main__"):
   unittest.main()
