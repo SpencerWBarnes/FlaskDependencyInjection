4f6feba34313370196c966b3a310f8920991265b
Spencer Barnes <interstellarjumper@gmail.com>
2020-12-02 16:39:00 -0600

    Used config in flask run

--- SampleApp/app.py
+++ SampleApp/app.py
@@ -1,14 +1,25 @@
 from flask import Flask, render_template, Response
 from DependencyContainer import DependencyContainer
-
 import appRoutes
+import config
+
+from dependency_injector.wiring import inject, Provide
+import sys
 
 def buildApp():
   dependencyContainer = DependencyContainer()
-  dependencyContainer.wire(modules=[appRoutes])
+  dependencyContainer.config.from_dict(config.config)
+
+  dependencyContainer.wire(modules=[sys.modules[__name__], appRoutes])
   appRoutes.app.container = dependencyContainer
   return appRoutes.app
 
+@inject
+def runApp(app,
+          appConfig = Provide[DependencyContainer.config.app]):
+  debug = (appConfig["env"] == "dev")
+  appRoutes.app.run(host=appConfig["host"], port=appConfig["port"], debug=debug)
+
 if __name__ == "__main__":
   appRoutes.app = buildApp()
-  appRoutes.app.run(host="0.0.0.0", debug=True)
+  runApp(appRoutes.app)
