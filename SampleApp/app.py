9601a4aef2d49e2a87970d45ef2c3291dfe1a2d2
Spencer Barnes <interstellarjumper@gmail.com>
2020-12-18 14:30:41 -0600

    Added a port override

--- SampleApp/app.py
+++ SampleApp/app.py
@@ -10,14 +10,19 @@ def buildApp():
   dependencyContainer = DependencyContainer()
   dependencyContainer.config.from_dict(config.config)
 
+  # List all modules containing an @inject decorator
   dependencyContainer.wire(modules=[sys.modules[__name__], appRoutes])
+  # Adding the container to the app gives the tests access to the container
   appRoutes.app.container = dependencyContainer
   return appRoutes.app
 
+# Decorator to inject something from a provider
 @inject
 def runApp(app,
           appConfig = Provide[DependencyContainer.config.app]):
-  debug = (appConfig["env"] == "dev")
+  debug = (appConfig["env"] == "dev") # Example use of using the dict from config["app"]
+  appConfig["port"] = 8080 # Example of how easy it is to overwrite a config at runtime
+  
   appRoutes.app.run(host=appConfig["host"], port=appConfig["port"], debug=debug)
 
 if __name__ == "__main__":
